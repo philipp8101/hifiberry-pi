d30c9463e5c964ebaa3555468338ff7d398c75df
Philipp Conrad <philipp8101@gmail.com>
2024-05-08 18:57:33 +0200

    setup scripts and config files for setting up hifiberry-dacplus on raspberry pi 3B+ with jellyfin and mopidy

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -e
+
+apt update -y
+apt upgrade -y
+apt install -y mopidy python3-pip pulseaudio
+
+curl https://repo.jellyfin.org/install-debuntu.sh |sed '136d' | bash
+
+pip3 install mopidy-jellyfin mopidy-musicbox-webclient --break-system-packages
+
+cp mopidy.conf /etc/mopidy/mopidy.conf
+cp config.txt /boot/config.txt
+cp default.pa /etc/pulse/default.pa
+
+systemctl enable mopidy
+
+reboot
+
